4cc682e6c93bac58461b154322a1d85604614466
reavil <reavil01@gmail.com>
2021-07-01 17:17:59 +0900

    백준 단계별로 풀어보기 기본 수학 문제
    - 문제 번호
    2775, 2839, 10757

--- src/main/kotlin/level/7/Level8.kt
+++ src/main/kotlin/level/7/Level8.kt
@@ -1,5 +1,6 @@
 package level.`7`
 
+import java.math.BigInteger
 import kotlin.math.ceil
 
 fun main() {
@@ -59,5 +60,51 @@ class Level8 {
             }
             println("${row}${"%02d".format(col)}")
         }
+    }
+
+    fun `백준 2775`() {
+        val iter = readLine()!!.toInt()
+        for (i in 1..iter) {
+            val k = readLine()!!.toInt()
+            val n = readLine()!!.toInt()
+
+            val arr = mutableListOf<IntArray>()
+            repeat(k + 1) { arr.add(IntArray(n) { 1 }) }
+
+            for (b in 0 until n) {
+                arr[0][b] += b
+            }
+
+            for (a in 1..k) {
+                for (b in 1 until n) {
+                    arr[a][b] = arr[a - 1][b] + arr[a][b - 1]
+                }
+            }
+            println(arr[k][n - 1])
+        }
+    }
+
+    fun `백준 2839`() {
+        val target = readLine()!!.toInt()
+        val remain = target % 5
+        val numOf5kg = target / 5
+
+        val result = when (remain) {
+            0 -> numOf5kg
+            1 -> if (numOf5kg - 1 >= 0) numOf5kg + 1 else -1
+            2 -> if (numOf5kg - 2 >= 0) numOf5kg + 2 else -1
+            3 -> numOf5kg + 1
+            4 -> if (numOf5kg - 1 >= 0) numOf5kg + 2 else -1
+            else -> -1
+        }
+        println(result)
+    }
+
+    fun `백준 10757`() {
+        val sum = readLine()!!
+            .split(" ")
+            .map { BigInteger(it) }
+            .sumOf { it }
+        println(sum)
     }
 }
