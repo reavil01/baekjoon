ba55d476083c2d784583e525db93307134af28b1
reavil <reavil01@gmail.com>
2021-07-21 19:40:33 +0900

    백준 단계별로 풀어보기 동적 계획법 1 문제
    - 문제 번호
    1003, 9184, 1904, 9461

---
+++ src/main/kotlin/level/7/Level15.kt
@@ -0,0 +1,122 @@
+package level.`7`
+
+fun main() {
+
+}
+
+class Level15 {
+    fun `백준 1003`() {
+        val reader = System.`in`.bufferedReader()
+        val writer = System.out.bufferedWriter()
+        val N = reader.readLine().toInt()
+
+        for (i in 1..N) {
+            val n = reader.readLine().toInt()
+            val memZero = IntArray(n + 1)
+            val memOne = IntArray(n + 1)
+            fibonacciCount(n, memZero, memOne)
+            writer.write("${memZero[n]} ${memOne[n]}\n")
+        }
+        writer.flush()
+    }
+
+    private fun fibonacciCount(n: Int, memZero: IntArray, memOne: IntArray) {
+        when (n) {
+            0 -> memZero[0] = 1
+            1 -> memOne[1] = 1
+            else -> {
+                if (memZero[n - 1] == 0 && memZero[n - 2] == 0) {
+                    fibonacciCount(n - 1, memZero, memOne)
+                    fibonacciCount(n - 2, memZero, memOne)
+                }
+                memZero[n] = memZero[n - 2] + memZero[n - 1]
+                memOne[n] = memOne[n - 2] + memOne[n - 1]
+            }
+        }
+    }
+
+    val mem = mutableMapOf<Int, Int>()
+
+    fun `백준 9184`() {
+        val reader = System.`in`.bufferedReader()
+        val writer = System.out.bufferedWriter()
+
+        while (true) {
+            val n = reader.readLine().split(" ").map { it.toInt() }
+            if (n.all { it == -1 }) break
+            val ans = w(n[0], n[1], n[2])
+            println("w(${n[0]}, ${n[1]}, ${n[2]}) = $ans")
+        }
+        writer.flush()
+    }
+
+    fun makeKey(a: Int, b: Int, c: Int): Int {
+        return a * 10000 + b * 100 + c
+    }
+
+    fun w(a: Int, b: Int, c: Int): Int {
+        val key = makeKey(a, b, c)
+        return when {
+            mem.containsKey(key) -> mem[key]!!
+            a <= 0 || b <= 0 || c <= 0 -> 1
+            a > 20 || b > 20 || c > 20 -> w(20, 20, 20)
+            a < b && b < c -> {
+                val ans = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c)
+                mem[key] = ans
+                ans
+            }
+            else -> {
+                val ans = w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1)
+                mem[key] = ans
+                ans
+            }
+        }
+    }
+
+    fun `백준 1904`() {
+        val reader = System.`in`.bufferedReader()
+        val writer = System.out.bufferedWriter()
+        val n = reader.readLine().toInt()
+        val mem = IntArray(n + 1)
+        val ans = tile(n, mem)
+        writer.write("${ans % 15746}")
+        writer.flush()
+    }
+
+    fun tile(n: Int, mem: IntArray): Int {
+        when (n) {
+            1 -> mem[1] = 1
+            2 -> mem[2] = 2
+            else -> {
+                if (mem[n - 1] == 0) tile(n - 1, mem)
+                if (mem[n - 2] == 0) tile(n - 2, mem)
+                mem[n] = (mem[n - 1] % 15746) + (mem[n - 2] % 15746)
+            }
+        }
+
+        return mem[n]
+    }
+
+    fun `백준 9461`() {
+        val reader = System.`in`.bufferedReader()
+        val writer = System.out.bufferedWriter()
+        val n = reader.readLine().toInt()
+        val mem = mutableListOf<Long>()
+        mem.addAll(listOf(1, 1, 1, 2, 2, 3, 4, 5, 7, 9))
+        repeat(n) {
+            val target = reader.readLine().toInt()
+            val ans = triangle(target - 1, mem)
+            writer.write("$ans\n")
+        }
+        writer.flush()
+    }
+
+    fun triangle(n: Int, mem: MutableList<Long>): Long {
+        if (mem.lastIndex < n) {
+            if (mem.lastIndex < n - 5) triangle(n - 5, mem)
+            if (mem.lastIndex < n - 1) triangle(n - 1, mem)
+            mem.add(mem[n - 1] + mem[n - 5])
+        }
+        return mem[n]
+    }
+}
