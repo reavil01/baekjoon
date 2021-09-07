3a85782f911d416309657605cdcaa6c02244cf9b
reavil <reavil01@gmail.com>
2021-09-07 19:41:41 +0900

    백준 단계별로 풀어보기 정수론 및 조합론 문제 완료
    - 문제 번호
    1010, 9375, 1676, 2004

--- src/main/kotlin/level/7/Level17.kt
+++ src/main/kotlin/level/7/Level17.kt
@@ -1,6 +1,35 @@
 package level.`7`
 
+import kotlin.math.min
 import kotlin.math.sqrt
+
+fun main() {
+    val reader = System.`in`.bufferedReader()
+    val writer = System.out.bufferedWriter()
+    val (n, m) = reader.readLine().split(" ").map { it.toLong() }
+
+    val a = getExponentOfNumFast(n, 5, 5)
+    val b = getExponentOfNumFast(m, 5, 5)
+    val c = getExponentOfNumFast((n - m), 5, 5)
+    val five = a - b - c
+
+    val d = getExponentOfNumFast(n, 2, 2)
+    val e = getExponentOfNumFast(m, 2, 2)
+    val f = getExponentOfNumFast((n - m), 2, 2)
+    val two = d - e - f
+
+    writer.write("${min(two, five)}\n")
+    writer.flush()
+}
+
+fun getExponentOfNumFast(n: Long, num: Long, unit: Long): Long {
+    if (n < num) return 0
+
+    var ans = n / num
+    ans += getExponentOfNumFast(n, num * unit, unit)
+
+    return ans
+}
 
 class Level17 {
     fun `백준 5086번`() {
@@ -176,5 +205,115 @@ class Level17 {
         }
 
         return mem[n][k]
+    }
+
+    fun `백준 1010번`() {
+        val reader = System.`in`.bufferedReader()
+        val writer = System.out.bufferedWriter()
+        val n = reader.readLine().toInt()
+        val inputs = Array(n) { reader.readLine().split(" ").map { it.toInt() } }
+
+        val table = makeBinomialCoefficientTable(30, 30)
+
+        inputs
+            .asSequence()
+            .forEach {
+                val ans = table[it[1]][it[0]]
+                writer.write("$ans\n")
+            }
+        writer.flush()
+    }
+
+    fun makeBinomialCoefficientTable(n: Int, k: Int): Array<IntArray> {
+        val mem = Array(n + 1) { IntArray(k + 1) }
+
+        for (i in 0..n) {
+            mem[i][0] = 1
+        }
+        for (i in 0..k) {
+            mem[i][i] = 1
+        }
+
+        for (i in 1..n) {
+            for (j in 1..k) {
+                mem[i][j] = mem[i - 1][j] + mem[i - 1][j - 1]
+            }
+        }
+
+        return mem
+    }
+
+    fun `백준 9375번`() {
+        val reader = System.`in`.bufferedReader()
+        val writer = System.out.bufferedWriter()
+        val n = reader.readLine().toInt()
+
+        repeat(n) {
+            val num = reader.readLine().toInt()
+            val inputs = HashMap<String, Int>()
+            repeat(num) {
+                val (item, type) = reader.readLine().split(" ")
+                if (inputs[type] == null) {
+                    inputs[type] = 1
+                } else {
+                    inputs[type] = inputs[type]!! + 1
+                }
+            }
+
+            val ans = inputs.values.fold(1) { acc, it -> acc * (it + 1) } - 1
+            writer.write("$ans\n")
+        }
+        writer.flush()
+    }
+
+    fun `백준 1676번`() {
+        val reader = System.`in`.bufferedReader()
+        val writer = System.out.bufferedWriter()
+        val n = reader.readLine().toInt()
+
+        var two = 0
+        var five = 0
+        for (i in 1..n) {
+            two += getExponentOfNum(i, 2)
+            five += getExponentOfNum(i, 5)
+        }
+
+        writer.write("${min(two, five)}\n")
+        writer.flush()
+    }
+
+    fun getExponentOfNum(n: Int, num: Int): Int {
+        if (n % num == 0) {
+            return getExponentOfNum(n / num, num) + 1
+        }
+        return 0
+    }
+
+    fun `백준 2004번`() {
+        val reader = System.`in`.bufferedReader()
+        val writer = System.out.bufferedWriter()
+        val (n, m) = reader.readLine().split(" ").map { it.toLong() }
+
+        val a = getExponentOfNumFast(n, 5, 5)
+        val b = getExponentOfNumFast(m, 5, 5)
+        val c = getExponentOfNumFast((n - m), 5, 5)
+        val five = a - b - c
+
+        val d = getExponentOfNumFast(n, 2, 2)
+        val e = getExponentOfNumFast(m, 2, 2)
+        val f = getExponentOfNumFast((n - m), 2, 2)
+        val two = d - e - f
+
+        writer.write("${min(two, five)}\n")
+        writer.flush()
+    }
+
+    fun getExponentOfNumFast(n: Long, num: Long, unit: Long): Long {
+        if (n < num) return 0
+
+        var ans = n / num
+        ans += getExponentOfNumFast(n, num * unit, unit)
+
+        return ans
     }
 }
