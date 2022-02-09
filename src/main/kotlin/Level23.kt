647b5e2b76381358ebf60f424372fd9e1435acff
reavil <reavil01@gmail.com>
2022-02-09 17:22:25 +0900

    백준 단계별로 풀어보기 동적 계획법 2
    - 문제 번호
    11049

--- src/main/kotlin/Level23.kt
+++ src/main/kotlin/Level23.kt
@@ -1,4 +1,5 @@
 fun main() {
+
 }
 
 class Level23 {
@@ -27,6 +28,34 @@ class Level23 {
 
             writer.write("${dp[0][n - 1]}\n")
         }
+        writer.flush()
+
+        reader.close()
+        writer.close()
+    }
+
+    fun `백준 11049번`() {
+        val reader = System.`in`.bufferedReader()
+        val writer = System.out.bufferedWriter()
+        val n = reader.readLine().toInt()
+
+        val matrix = Array(n) {
+            reader.readLine().split(" ").map { it.toInt() }
+        }
+
+        val dp = Array(n) { x -> IntArray(n) { y -> if (x == y) 0 else Int.MAX_VALUE } }
+
+        for (d in 1 until n) {
+            for (i in 0 until n) {
+                val j = i + d
+                if (j >= n) continue
+
+                dp[i][j] = (i until j).minOf { k ->
+                    dp[i][k] + dp[k + 1][j] + matrix[i][0] * matrix[k][1] * matrix[j][1]
+                }
+            }
+        }
+        writer.write("${dp[0][n - 1]}")
         writer.flush()
 
         reader.close()
